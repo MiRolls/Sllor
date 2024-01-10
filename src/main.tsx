85e9dba254d5302036c96d5769fc0035dcdcd1ef
liangmiQwQ <mi_yyds@126.com>
2024-01-10 19:18:54 +0800

    dependencies: radix, postcss, tailwindcss and more

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './components/App/App.tsx'
 import './index.css'
+import '@radix-ui/themes/styles.css';
+import {Theme} from "@radix-ui/themes";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <Theme accentColor={"red"}>
+          <App />
+      </Theme>
   </React.StrictMode>,
 )
