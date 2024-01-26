f88eb6c25c0b0ee906afd12263c4521d25b9b0a4
liangmiQwQ <mi_yyds@126.com>
2024-01-26 18:27:42 +0800

    fix: fix site.ts type bug

--- src/store/site.ts
+++ src/store/site.ts
@@ -1,24 +1,27 @@
-import {create} from "zustand";
-import {Site} from "../interfaces/site";
+import { create } from "zustand";
+import { Site } from "../interfaces/site";
 
 export interface SiteState {
-    site: Site,
-    changeSite: (value: Site) => void
+	site: Site;
+	changeSite: (value: Site) => void;
 }
 
-export const useSite = create((set): SiteState => ({
-    site: {
-        name: "",
-        "site-url": "",
-        "main-color": "",
-        lang: "enUs",
-        logo: "",
-        introduce: "",
-        about: "",
-        footer: ""
-    },
-    changeSite: (value: Site) => set(() => ({
-        site: value
-    }))
-}))
-
+export const useSite = create(
+	(set): SiteState => ({
+		site: {
+			name: "",
+			"site-url": "",
+			"main-color": "",
+			lang: "enUs",
+			logo: "",
+			introduce: "",
+			about: "",
+			footer: "",
+			"short-introduce": "",
+		},
+		changeSite: (value: Site) =>
+			set(() => ({
+				site: value,
+			})),
+	})
+);
