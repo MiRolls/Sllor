b98e68f96158f00dfe63067d55f9faf94cad01f6
liangmiQwQ <mi_yyds@126.com>
2024-01-28 10:48:41 +0800

    feat: SSR main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+"use server";
 import IndexTitle from "@/components/IndexTitle";
 import { Box } from "@radix-ui/themes";
 import Markdown from "@/components/Markdown";
 import "@/style/IndexPage.css";
 
-export default function Home() {
+export default async function Home() {
     const string = `# I love Markdown \n\n\n 1231242112 \n\n1231242112 \n\n1231242112 \n\n1231242112 \n\n1231242112 \n\n1231242112 \n\n1231242112 \n\n1231242112 \n\n1231242112 \n\n1231242112 \n\n 1231123r21342q13122214412342rewf324rewfw34wegw34rweg432rwegsfbdtw43gress4w3rt4grew4t3tgresdew恶搞人生问题4俄国人淘宝达人国伟图43俄国人沙特4问题3俄国人是不同的人沟通43俄国人不同的人格问题4个人吧rfgbfdxerfrgbdfregbfgdresfrgbdrsegdesgdbgf242112`;
 
     return (
