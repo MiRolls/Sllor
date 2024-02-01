34eaec36ef0e87cb2c8103113e851a1275a74498
liangmiQwQ <mi_yyds@126.com>
2024-02-01 20:22:18 +0800

    fix: The control style bug and the NavBar's bug

--- src/components/Control.tsx
+++ src/components/Control.tsx
@@ -5,49 +5,49 @@ import { AnimatePresence, motion } from "framer-motion";
 import { ControlState, useControl } from "../store/control";
 
 const Control = () => {
-	const control = useControl(state => (state as ControlState).control);
-	const show = useControl(state => (state as ControlState).show);
-	return (
-		<AnimatePresence mode="wait">
-			{control.length > 0 && show && (
-				<motion.div
-					initial={{ x: "-100%" }}
-					animate={{ x: 0 }}
-					exit={{ x: "-100%" }}
-					className="h-full w-fix lg:w-1/5 md:w-4/12 w-4/5 fixed"
-					transition={{
-						damping: 10,
-						stiffness: 100,
-						duration: 0.2,
-					}}
-				>
-					<Flex
-						direction="column"
-						className="p-3 gap-1 pt-2 flex bg-accent w-full h-full border-t-2 border-accent-800"
-					>
-						{control.map((item, index) => {
-							return (
-								<Button
-									key={item.toString() + index}
-									className={classNames(
-										"p-10 !py-4 flex flex-nowrap !h-10 !justify-start",
-										{ "!bg-accent-800": item.active }
-									)}
-									onClick={item.action}
-									radius="full"
-								>
-									{React.cloneElement(item.icon, {
-										className: "h-6 w-6 ml-2",
-									})}
-									<Text>{item.name}</Text>
-								</Button>
-							);
-						})}
-					</Flex>
-				</motion.div>
-			)}
-		</AnimatePresence>
-	);
+    const control = useControl(state => (state as ControlState).control);
+    const show = useControl(state => (state as ControlState).show);
+    return (
+        <AnimatePresence mode="wait">
+            {control.length > 0 && show && (
+                <motion.div
+                    initial={{ x: "-100%" }}
+                    animate={{ x: 0 }}
+                    exit={{ x: "-100%" }}
+                    className="h-full w-fix lg:w-1/5 md:w-4/12 w-4/5 fixed"
+                    transition={{
+                        damping: 10,
+                        stiffness: 100,
+                        duration: 0.2,
+                    }}
+                >
+                    <Flex
+                        direction="column"
+                        className="p-3 gap-1 pt-4 flex bg-none w-full h-full border-r-[1px] border-slate-100"
+                    >
+                        {control.map((item, index) => {
+                            return (
+                                <Button
+                                    key={item.toString() + index}
+                                    className={classNames(
+                                        "p-10 !py-4 flex flex-nowrap !h-10 !justify-start !bg-transparent hover:!bg-slate-200",
+                                        { "!bg-accent-800": item.active }
+                                    )}
+                                    onClick={item.action}
+                                    radius="full"
+                                >
+                                    {React.cloneElement(item.icon, {
+                                        className: "h-6 w-6 ml-2",
+                                    })}
+                                    <Text>{item.name}</Text>
+                                </Button>
+                            );
+                        })}
+                    </Flex>
+                </motion.div>
+            )}
+        </AnimatePresence>
+    );
 };
 
 export default Control;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,138 +11,140 @@ import { useTranslation } from "react-i18next";
 import { ControlState, useControl } from "@/store/control";
 
 const NavBar = () => {
-	const site = useSite(state => (state as SiteState).site);
-	const [t, _] = useTranslation();
+    const site = useSite(state => (state as SiteState).site);
+    const [t, _] = useTranslation();
 
-	// Get dark api
-	const stateDark = useDark(state => (state as DarkState).dark);
-	const changeDark: any = useDark(state => (state as DarkState).changeDark);
+    // Get dark api
+    const stateDark = useDark(state => (state as DarkState).dark);
+    const changeDark: any = useDark(state => (state as DarkState).changeDark);
 
-	const changeDarkAndStorage = () => {
-		changeDark();
-	};
+    const changeDarkAndStorage = () => {
+        changeDark();
+    };
 
-	const changeShow = useControl(state => (state as ControlState).changeShow);
+    const changeShow = useControl(state => (state as ControlState).changeShow);
 
-	function chgShow() {
-		changeShow();
-	}
+    function chgShow() {
+        changeShow();
+    }
 
-	useUpdateEffect(() => {
-		localStorage.setItem("dark", stateDark);
-	}, [stateDark]);
+    useUpdateEffect(() => {
+        localStorage.setItem("dark", stateDark);
+    }, [stateDark]);
 
-	return (
-		<Flex
-			className={"items-center bg-accent fixed w-full z-20"}
-			justify="between"
-			height={"9"}
-			p={"3"}
-		>
-			{/* Menu button */}
-			<Box>
-				<IconButton
-					radius="full"
-					size="3"
-					// className={"outline-none duration-200 flex items-center justify-center w-12 h-12"}>
-					className="duration-200 outline-none"
-					onClick={chgShow}
-				>
-					<HiMenu color={stateDark === "dark" ? "white" : "black"} className="w-6 h-6" />
-				</IconButton>
-				{/* Just support css */}
-				<IconButton size="3" className="opacity-0 sm:!hidden" />
-			</Box>
-			{/* Site info(Click to go to indexPage) */}
-			<Link href="/" className="block">
-				{/* Site Logo */}
-				<Avatar
-					src={site.logo}
-					fallback={site.name}
-					size="2"
-					className={"sm:ml-1"}
-				></Avatar>
-			</Link>
+    return (
+        <Flex
+            className={"items-center bg-accent fixed w-full z-20"}
+            justify="between"
+            height={"9"}
+            p={"3"}
+        >
+            {/* Menu button */}
+            <Box>
+                <IconButton
+                    radius="full"
+                    size="3"
+                    // className={"outline-none duration-200 flex items-center justify-center w-12 h-12"}>
+                    className="duration-200 outline-none"
+                    onClick={chgShow}
+                >
+                    <HiMenu color={stateDark === "dark" ? "white" : "black"} className="w-6 h-6" />
+                </IconButton>
+                {/* Just support css */}
+                <IconButton size="3" className="opacity-0 sm:!hidden" />
+            </Box>
+            {/* Site info(Click to go to indexPage) */}
+            <Link href="/" className="block">
+                {/* Site Logo */}
+                <Avatar
+                    src={site.logo}
+                    fallback={site.name}
+                    size="2"
+                    className={"sm:ml-1"}
+                ></Avatar>
+            </Link>
 
-			<Link href="/" className="ml-2 sm:block sm:ml-3 hidden flex-1">
-				{/* Site Name */}
-				<Heading>{site.name}</Heading>
-			</Link>
+            <Box className="ml-2 sm:block sm:ml-3 hidden flex-1">
+                <Link href="/">
+                    {/* Site Name */}
+                    <Heading className="!w-auto inline">{site.name}</Heading>
+                </Link>
+            </Box>
 
-			<Box className={"hidden sm:block"}>
-				<Link href="/about">
-					<Button>{t("About")}</Button>
-				</Link>
-				<Link href="/questionnaire">
-					<Button>{t("Home")}</Button>
-				</Link>
-			</Box>
-			{/* Tool Buttons */}
-			<Box className="">
-				<IconButton
-					onClick={changeDarkAndStorage}
-					radius="full"
-					size="3"
-					className={"outline-none duration-200"}
-				>
-					{stateDark === "light" && (
-						<motion.div
-							exit={{ rotate: 180, scale: 0.9 }}
-							initial={{ rotate: 180, scale: 0.9 }}
-							animate={{ rotate: 0, scale: 1 }}
-						>
-							<CiDark
-								color={stateDark === "light" ? "black" : "white"}
-								className="w-6 h-6 stroke-1"
-							/>
-						</motion.div>
-					)}
-					{stateDark === "dark" && (
-						<motion.div
-							initial={{ rotate: 180, scale: 0.9 }}
-							animate={{ rotate: 0, scale: 1 }}
-							exit={{ rotate: 180, scale: 0.9 }}
-						>
-							<CiLight
-								color={stateDark === "dark" ? "white" : "black"}
-								className="w-6 h-6 stroke-1"
-							/>
-						</motion.div>
-					)}
-				</IconButton>
+            <Box className={"hidden sm:block"}>
+                <Link href="/about">
+                    <Button>{t("About")}</Button>
+                </Link>
+                <Link href="/questionnaire">
+                    <Button>{t("Home")}</Button>
+                </Link>
+            </Box>
+            {/* Tool Buttons */}
+            <Box className="">
+                <IconButton
+                    onClick={changeDarkAndStorage}
+                    radius="full"
+                    size="3"
+                    className={"outline-none duration-200"}
+                >
+                    {stateDark === "light" && (
+                        <motion.div
+                            exit={{ rotate: 180, scale: 0.9 }}
+                            initial={{ rotate: 180, scale: 0.9 }}
+                            animate={{ rotate: 0, scale: 1 }}
+                        >
+                            <CiDark
+                                color={stateDark === "light" ? "black" : "white"}
+                                className="w-6 h-6 stroke-1"
+                            />
+                        </motion.div>
+                    )}
+                    {stateDark === "dark" && (
+                        <motion.div
+                            initial={{ rotate: 180, scale: 0.9 }}
+                            animate={{ rotate: 0, scale: 1 }}
+                            exit={{ rotate: 180, scale: 0.9 }}
+                        >
+                            <CiLight
+                                color={stateDark === "dark" ? "white" : "black"}
+                                className="w-6 h-6 stroke-1"
+                            />
+                        </motion.div>
+                    )}
+                </IconButton>
 
-				{/* Phone dropdown menu */}
-				<Box className="sm:hidden inline">
-					<DropdownMenu.Root>
-						<DropdownMenu.Trigger>
-							<IconButton
-								radius="full"
-								size="3"
-								className={"outline-none duration-200"}
-							>
-								<IoMdMore
-									className="w-6 h-6 stroke-1"
-									color={stateDark === "dark" ? "white" : "black"}
-								/>
-							</IconButton>
-						</DropdownMenu.Trigger>
-						<DropdownMenu.Content>
-							<Link href="/about">
-								<DropdownMenu.Item className="!bg-transparent">
-									{t("About")}
-								</DropdownMenu.Item>
-							</Link>
-							<Link href="/questionnaire">
-								<DropdownMenu.Item className="!bg-transparent">
-									{t("Home")}
-								</DropdownMenu.Item>
-							</Link>
-						</DropdownMenu.Content>
-					</DropdownMenu.Root>
-				</Box>
-			</Box>
-		</Flex>
-	);
+                {/* Phone dropdown menu */}
+                <Box className="sm:hidden inline">
+                    <DropdownMenu.Root>
+                        <DropdownMenu.Trigger>
+                            <IconButton
+                                radius="full"
+                                size="3"
+                                className={"outline-none duration-200"}
+                            >
+                                <IoMdMore
+                                    className="w-6 h-6 stroke-1"
+                                    color={stateDark === "dark" ? "white" : "black"}
+                                />
+                            </IconButton>
+                        </DropdownMenu.Trigger>
+                        <DropdownMenu.Content>
+                            <Link href="/about">
+                                <DropdownMenu.Item className="!bg-transparent">
+                                    {t("About")}
+                                </DropdownMenu.Item>
+                            </Link>
+                            <Link href="/questionnaire">
+                                <DropdownMenu.Item className="!bg-transparent">
+                                    {t("Home")}
+                                </DropdownMenu.Item>
+                            </Link>
+                        </DropdownMenu.Content>
+                    </DropdownMenu.Root>
+                </Box>
+            </Box>
+        </Flex>
+    );
 };
 
 export default NavBar;
