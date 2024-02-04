b88c667d45f3524ae3e549867f898afa69fddd8e
liangmiQwQ <mi_yyds@126.com>
2024-02-04 09:59:31 +0800

    fix: Remove unused imports and code

--- src/app/questionnaire/page.tsx
+++ src/app/questionnaire/page.tsx
@@ -1,11 +1,7 @@
 "use client";
 import { useEffect } from "react";
 import { ControlState, useControl } from "@/store/control";
-import { HiMenu } from "react-icons/hi";
-import { IoCreateOutline, IoCreateSharp } from "react-icons/io5";
 import isPhone from "@/utils/isPhone";
-import { t } from "i18next";
-import { P } from "@/components/Markdown/children/Text";
 
 const QuestionnairePage = () => {
     const changeControl = useControl(state => (state as ControlState).changeControl);
