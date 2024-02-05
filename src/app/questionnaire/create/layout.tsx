dc06cce9ec9ed3177a308d92f56ab6c31d64f8a2
liangmiQwQ <mi_yyds@126.com>
2024-02-05 12:16:15 +0800

    feat: Refactor layout components and add new icons

--- src/app/questionnaire/create/layout.tsx
+++ src/app/questionnaire/create/layout.tsx
@@ -1,8 +1,22 @@
 "use client";
-import React, { ReactNode, useEffect } from "react";
+import { ReactNode, useEffect } from "react";
+import { useControl, ControlState } from "../../../store/control";
 
 const CreateLayout = ({ children }: { children: ReactNode }) => {
-    useEffect(() => {}, []);
+    const originalIsShow = (useControl() as ControlState).show;
+    // temp value
+    const { changeShow } = useControl() as ControlState;
+
+    useEffect(() => {
+        changeShow(false);
+
+        return () => {
+            changeShow(originalIsShow);
+        };
+
+        // never change
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     return children;
 };
 
--- src/app/questionnaire/layout.tsx
+++ src/app/questionnaire/layout.tsx
@@ -3,19 +3,33 @@ import { ReactNode, useEffect } from "react";
 import { ControlState, useControl } from "@/store/control";
 import isPhone from "@/utils/isPhone";
 import { t } from "i18next";
-import { FaRegUser } from "react-icons/fa";
+import { FiUser } from "react-icons/fi";
+import { useRouter } from "next/navigation";
+import { IoAdd } from "react-icons/io5";
 
 const QUestionnaireLayout = ({ children }: { children: ReactNode }) => {
     const changeControl = useControl(state => (state as ControlState).changeControl);
     const clear = useControl(state => (state as ControlState).clear);
     const changeShow = useControl(state => (state as ControlState).changeShow);
+    const router = useRouter();
 
     useEffect(() => {
         changeControl([
             {
                 type: "choice",
                 name: t("My"),
-                icon: <FaRegUser />,
+                icon: <FiUser />,
+                action: () => {
+                    router.push("/questionnaire/");
+                },
+            },
+            {
+                type: "choice",
+                name: t("Create"),
+                icon: <IoAdd />,
+                action: () => {
+                    router.push("/questionnaire/create");
+                },
             },
         ]);
         return () => {
