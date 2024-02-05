ac91857b32845a7921bad703543acad67407d0e4
liangmiQwQ <mi_yyds@126.com>
2024-02-05 20:48:15 +0800

    feat: add 'add question' button

--- src/app/questionnaire/create/_components/Main.tsx
+++ src/app/questionnaire/create/_components/Main.tsx
@@ -1,10 +1,12 @@
 "use client";
-import { Flex, TextField } from "@radix-ui/themes";
+import { Box, Button, Flex, IconButton, Text, TextField } from "@radix-ui/themes";
 import React from "react";
+import { IoMdAdd } from "react-icons/io";
+import { Question } from "../../../../interfaces/questionnaire";
 
 export default function Main() {
     return (
-        <Flex className="p-10 sm:p-32" direction={"column"} align={"center"}>
+        <Flex className="p-10 sm:p-32 min-h-full w-full" direction={"column"} align={"center"}>
             <textarea
                 placeholder="Please enter your title"
                 className="[resize:none] !border-none !outline-none h-auto block font-bold !text-2xl sm:!text-4xl w-full text-center"
@@ -13,7 +15,15 @@ export default function Main() {
                         event.preventDefault();
                     }
                 }}
-            ></textarea>
+            />
+            <Box className="mt-5 block">
+                <Button size={"3"} radius="full" className="!text-white">
+                    <Flex gap="1">
+                        <IoMdAdd size={25} className=" text-white mr-0" />
+                        <Text>Add Question</Text>
+                    </Flex>
+                </Button>
+            </Box>
         </Flex>
     );
 }
