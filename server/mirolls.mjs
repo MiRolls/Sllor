f6dd0b314a75c7c72e1ff15f7e1baf87a38e3080
liangmiQwQ <mi_yyds@126.com>
2024-02-17 13:29:58 +0800

    style: format

--- server/mirolls.mjs
+++ server/mirolls.mjs
@@ -1,25 +1,24 @@
-import express from "express"
-import fs from "fs"
+import express from "express";
+import fs from "fs";
 
-const app = express()
-const port = 2333
-const jsonfilePath = 'server/mirolls.json'
-let jsonObject
+const app = express();
+const port = 2333;
+const jsonfilePath = "server/mirolls.json";
+let jsonObject;
 
-fs.readFile(jsonfilePath, 'utf8', (err, data) => {
+fs.readFile(jsonfilePath, "utf8", (err, data) => {
     if (err) {
-        console.error('error:', err);
+        console.error("error:", err);
         return;
     }
     jsonObject = JSON.parse(data);
-})
+});
 
-
-app.post('/site/get', (req, res) => {
-    console.log(`Access get site`)
-    res.send(JSON.stringify(jsonObject["get-site"]))
-})
+app.post("/site/get", (req, res) => {
+    console.log(`Access get site`);
+    res.send(JSON.stringify(jsonObject["get-site"]));
+});
 
 app.listen(port, () => {
-    console.log(`Model server is running at ${port}`)
-})
+    console.log(`Model server is running at ${port}`);
+});
